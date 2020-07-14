5e0ee6413103fc5479656e81b9bd8b3d69ad69d5
Angus <34198794+angusmc90@users.noreply.github.com>
2020-07-13 20:19:45 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <span>&nbsp;</span>
+    // <div className="App">
+    //   <h1>Hello CodeSandbox</h1>
+    //   <h2>Start editing to see some magic happen!</h2>
+    // </div>
+  );
+}
